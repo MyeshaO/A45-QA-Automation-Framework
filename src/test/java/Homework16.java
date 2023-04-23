83ded6a69499c3cc9ed17b5d560d83cba93d6402
MyeshaO <130818751+MyeshaO@users.noreply.github.com>
2023-04-23 14:15:56 -0700

    HW updates
    updates made from hw team suggestions. Thanks!

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -5,27 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.testng.annotations.BeforeSuite;
-import org.testng.TestNG;
-
 import java.time.Duration;
-
-
 public class Homework16 extends BaseTest{
     @Test
-
     public void registrationNavigation(){
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://bbb.testpro.io/";
         driver.get(url);
-
         WebElement registrationLink = driver.findElement(By.cssSelector("[id='hel']"));
         registrationLink.click();
-
         String registrationURL = "https://bbb.testpro.io/registration.php";
         Assert.assertEquals(driver.getCurrentUrl(),registrationURL);
+        driver.quit();
     }
 }
